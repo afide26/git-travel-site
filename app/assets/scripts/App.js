52fa18c8937dd136533016433838ba270d01ac52
Alan Fidelino <afidedev26@gmail.com>
2017-08-26 23:26:25 +1000

    Make RevealOnScroll reusable

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
-var revealonScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "65%");
